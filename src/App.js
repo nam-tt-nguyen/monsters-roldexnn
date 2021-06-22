6982ceb67c8502dfb718b10da28739d0ffd91036
nnguyen <nam.nguyen@justice.vic.gov.au>
2021-06-22 12:28:57 +1000

    adding file for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import { SearhBox } from './components/search-box/search-box.component';
+
 import './App.css';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  // Using componentDidMount() to fetch data from the URL, 
+  // then convert it to json format so JavaScript can understand and use
+  // then assign users list to the monsters array
+ componentDidMount(){ 
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response => response.json())
+   .then(users => this.setState({monsters: users}));
+
+ }
+
+ handleChange = e => {
+   this.setState({searchField: e.target.value});
+ }
+
+  render(){
+    const {monsters, searchField}= this.state;
+    // The above Destructuring equivalent to the following:
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearhBox 
+          placeholder='search monster'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>       
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //import from App.js
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// ReactDom library calls render to render our App function in the form of HTML
+// document.getElementById('root'): looking on the document object for some elements with 
+// an id of root & it replaces it with that HTML from our "App" function
+ReactDOM.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
